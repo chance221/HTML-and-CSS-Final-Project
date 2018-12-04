3e04477bf68f09230232afb0053c32e682016b83
chance221 <39056884+chance221@users.noreply.github.com>
2018-12-03 20:09:05 -0500

    Add files via upload

---
+++ FinalProject/js/js.js
@@ -0,0 +1,5 @@
+window.alert("Welcome To My Final Project");
+onunload=("Thank you for Visiting My Page")
+
+var newColor=document.getAnimationsElementById("favColor").value;
+document.getElementById('altColor'.style.color=newColor);
